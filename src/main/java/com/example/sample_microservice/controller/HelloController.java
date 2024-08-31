cc1392d199e004deeea78854177425ef08df6405
Vaibhav <vsinghal85@gmail.com>
2024-08-31 18:54:22 +0530

    Changing message for testing

--- src/main/java/com/example/sample_microservice/controller/HelloController.java
+++ src/main/java/com/example/sample_microservice/controller/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello from AWS Fargate!";
+        return "Hello from AWS Fargate! We have code pipeline successfully setup";
     }
 }
